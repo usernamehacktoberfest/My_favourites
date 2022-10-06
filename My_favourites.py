3edd7fbaac59ae0dd13f56d38c91bf40be503bc0
Umakshi Sharma <66000862+Umakshi12@users.noreply.github.com>
2022-10-07 01:23:24 +0530

    Added My_favourites.py
    This file contain all the answers of given question in python language.

---
+++ My_favourites.py
@@ -0,0 +1,22 @@
+# here is the answer of every question
+
+n: str = input("Enter your Name.")
+country = input("Enter your Country.")
+link= input("Can we connect on linkedin?")
+lang = input("Your favourite Programming language?")
+web = input("Your favourite website?")
+app = input("Your favourite app?")
+
+print("Hii, My Name is {}".format(n))
+print("I am from {}".format(country))
+print("Yes my link is {}".format(link))
+print("My favourite programming language is {}".format(lang))
+print("My favourite website is {}".format(web))
+print("My favourite application is {}".format(app))
+
+if __name__ == "main":
+    n = "Umakshi"
+    country = "India"
+    link = "www.linkedin.com/in/umakshi-sharma-163302206"
+    web = "CodeStudio"
+    app = "PyCharm"
